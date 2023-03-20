c372358d29b504a715a363100e59b8f00ef5e464
Sourcery AI <>
2023-03-20 19:48:17 +0000

    'Refactored by Sourcery'

--- init.py
+++ init.py
@@ -11,24 +11,23 @@ def initiate(configfile, *args):
         parser = ConfigParser.ConfigParser()
         parser.read(configfile)
         for arg in args:
-            if parser.has_section(arg):
-                params = parser.items(arg)
-                gendict = {}
-                for param in params:
-                    if isinstance(param[1], numbers.Integral):
-                        gendict[param[0]] = parser.getint(arg, param[0])
-                    elif isinstance(param[1], float):
-                        gendict[param[0]] = parser.getfloat(arg, param[0])
-                    elif isinstance(param[1], bool):
-                        gendict[param[0]] = parser.getboolean(arg, param[0])
-                    else:
-                        gendict[param[0]] = param[1]
-                dictlist.append(gendict)
-            else:
+            if not parser.has_section(arg):
                 raise Exception('Section {0} not found in the {1} file'.format(arg, configfile))
+            params = parser.items(arg)
+            gendict = {}
+            for param in params:
+                if isinstance(param[1], numbers.Integral):
+                    gendict[param[0]] = parser.getint(arg, param[0])
+                elif isinstance(param[1], float):
+                    gendict[param[0]] = parser.getfloat(arg, param[0])
+                elif isinstance(param[1], bool):
+                    gendict[param[0]] = parser.getboolean(arg, param[0])
+                else:
+                    gendict[param[0]] = param[1]
+            dictlist.append(gendict)
         return dictlist
     except Exception as err:
-        print("FATAL ERROR! Failed to get config info: %s" % (str(err)))
-        print("Config file: %s" % str(configfile))
-        print("Arg: %s" % str(arg) for arg in args)
+        print(f"FATAL ERROR! Failed to get config info: {str(err)}")
+        print(f"Config file: {str(configfile)}")
+        print(f"Arg: {str(arg)}" for arg in args)
         sys.exit(1)
